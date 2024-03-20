fa3a5b3e4575e1da2769e21104e977d4d1304aaf
Juncheng Liu <juncheng.liu@junchen-ltmqtex.internal.salesforce.com>
2024-03-20 20:00:16 +0800

    evaluate all lsf datasets in a file

--- cli/eval.py
+++ cli/eval.py
@@ -18,15 +18,70 @@ import torch
 from gluonts.time_feature import get_seasonality
 from hydra.core.hydra_config import HydraConfig
 from hydra.utils import call, instantiate
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from torch.utils.tensorboard import SummaryWriter
 
 from uni2ts.common import hydra_util  # noqa: hydra resolvers
 from uni2ts.eval_util.evaluation import evaluate_model
 
 
+def evaluate(cfg):
+    # print(OmegaConf.to_yaml(cfg))
+    test_data, metadata = call(cfg.data)
+    batch_size = cfg.batch_size
+    while True:
+        model = call(cfg.load_from_checkpoint)(
+            prediction_length=metadata.prediction_length,
+            checkpoint_path=call(cfg.checkpoint_path),
+            target_dim=metadata.target_dim,
+            feat_dynamic_real_dim=metadata.feat_dynamic_real_dim,
+            past_feat_dynamic_real_dim=metadata.past_feat_dynamic_real_dim,
+            context_length=cfg.context_length,
+            patch_size=cfg.patch_size,
+        )
+        metrics = instantiate(cfg.metrics, _convert_="all")
+        try:
+            predictor = model.create_predictor(batch_size, cfg.device)
+            res = evaluate_model(
+                predictor,
+                test_data=test_data,
+                metrics=metrics,
+                batch_size=cfg.batch_size,
+                axis=None,
+                mask_invalid_label=True,
+                allow_nan_forecast=False,
+                seasonality=get_seasonality(metadata.freq),
+            )
+            print(res)
+            return res
+            # output_dir = HydraConfig.get().runtime.output_dir
+            # writer = SummaryWriter(
+            #     log_dir=os.path.join(
+            #         output_dir,
+            #         f"patch_size={cfg.patch_size}",
+            #         f"context_length={cfg.context_length}",
+            #     )
+            # )
+            # for name, metric in res.to_dict("records")[0].items():
+            #     writer.add_scalar(f"{metadata.split}_metrics/{name}", metric)
+            # writer.close()
+            break
+        except torch.cuda.OutOfMemoryError:
+            print(
+                f"OutOfMemoryError at batch_size {batch_size}, reducing to {batch_size//2}"
+            )
+            batch_size //= 2
+            if batch_size < cfg.min_batch_size:
+                print(
+                    f"batch_size {batch_size} smaller than min_batch_size {cfg.min_batch_size}, "
+                    f"ending evaluation of patch_size {cfg.patch_size}, context_length {cfg.context_length}"
+                )
+                break
+
+
 @hydra.main(version_base="1.3", config_path="conf/eval", config_name="default")
 def main(cfg: DictConfig):
+    # print(OmegaConf.to_yaml(cfg))
     test_data, metadata = call(cfg.data)
     batch_size = cfg.batch_size
     while True:
---
+++ cli/hydra_eval.py
@@ -0,0 +1,70 @@
+# from cli import eval
+import os
+import time
+
+import ipdb
+from eval import evaluate
+from hydra import compose, initialize
+from omegaconf import OmegaConf
+
+if __name__ == "__main__":
+    print("start")
+    initialize(version_base=None, config_path="conf/eval")
+    dataset_names = ["ETTh1", "ETTh2", "ETTm1", "ETTm2", "electricity", "weather"]
+    patch_sz = {
+        "ETTh1": 64,
+        "ETTh2": 64,
+        "ETTm1": 128,
+        "ETTm2": 64,
+        "electricity": 64,
+        "weather": 128,
+    }
+    context_lengths = {
+        "ETTh1": 5000,
+        "ETTh2": 3000,
+        "ETTm1": 4000,
+        "ETTm2": 3000,
+        "electricity": 5000,
+        "weather": 2000,
+    }
+    time_dict = {}
+    res_dict = {}
+    dir_path = "./eval_results"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    for dataset_name in dataset_names:
+        print(f"----- Dataset Name: {dataset_name} -----")
+        cur_log_file = open(os.path.join(dir_path, dataset_name + ".log"), "a")
+        tot_t1 = time.time()
+        for pred_len in ["96", "192", "336", "720"]:
+            print(f"----- pred length: {pred_len} -----")
+            cur_t1 = time.time()
+            cfg = compose(
+                config_name="default",
+                overrides=[
+                    "data=lsf_test",
+                    f"data.dataset_name={dataset_name}",
+                    "data.mode=M",
+                    f"patch_size={patch_sz[dataset_name]}",
+                    'device="cuda"',
+                    f"context_length={context_lengths[dataset_name]}",
+                    f"data.prediction_length={pred_len}",
+                    "checkpoint_path=moirai_R_small",
+                ],
+            )
+            res = evaluate(cfg)
+            if dataset_name not in time_dict:
+                time_dict[dataset_name] = {}
+                res_dict[dataset_name] = {}
+            time_dict[dataset_name][pred_len] = time.time() - cur_t1
+            res_dict[dataset_name][pred_len] = res
+            print(
+                f'pred_len: {pred_len}, MSE[0.5]: {res["MSE[0.5]"].item()}, MAE[0.5]: {res["MAE[0.5]"].item()}'
+            )
+            cur_log_file.write(
+                f'pred_len: {pred_len}, MSE[0.5]: {res["MSE[0.5]"].item()}, MAE[0.5]: {res["MAE[0.5]"].item()}\n'
+            )
+        tot_endt = time.time()
+        print(f"Total Time elapsed for {dataset_name}: {tot_endt-tot_t1}s")
+        print("individual time for each dataset")
+        print(time_dict[dataset_name])
